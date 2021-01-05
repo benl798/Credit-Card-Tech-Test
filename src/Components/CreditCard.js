9de4e392350c31e1b15a33fd795b51d787090812
benl798 <ben.l7@hotmail.com>
2021-01-06 01:15:32 +1100

    Dropdown date picker

--- src/Components/CreditCard.js
+++ src/Components/CreditCard.js
@@ -16,12 +16,13 @@ class CreditCard extends Component {
 constructor(props) {
     super(props);
     this.state = {
-    title: null,
-    number: null,
-    month: null,
-    year: null,
-    cvc: null,
-    disable: true,
+    title: "",
+    number: "",
+    month: "",
+    year: "",
+    cvc: "",
+    selectMonth: "",
+    selectYear: "",
     errors: {
         title: "",
         number: "",
@@ -30,18 +31,9 @@ constructor(props) {
         cvc: ""
         }
     };
-    } // constructor
 
-// typeOfNaN = (event) => 
-//     event.preventDefault(); 
-//     if (Number.isNaN()) {
-//         return "Number NaN";
-//     } 
-//     if (isNaN()) {
-//         return "NaN";
-//     }
-
-
+    this.handleDropdownMonth = this.handleDropdownMonth.bind(this);
+} // constructor
 
 handleChange = (event) => {
     event.preventDefault();
@@ -58,21 +50,36 @@ handleChange = (event) => {
           value.trim().length < 5 ? "Your card name must be at least 5 characters long" : "";
         break;
       case "number": 
-        errors.title = 
-          value.trim().length < 16 ? "Card number must be 16 digits long" : "";
+        errors.number = 
+          value.trim().length < 16 || !Number(value) ? "Card number must be 16 digits long and a number" : "";
         break;
-      case "month": 
+      case "month":
         errors.month = 
-          value.
-        //   value.length < 2 ? "Wrong input muppet" : "";
+          value.trim().length < 2 || !Number(value) ? "This input accepts numerical values only, please enter a valid date" : "";
         break;
-      default:
+      case "year":
+        errors.year = 
+          value.trim().length < 2 || !Number(value) ? "This input accepts numerical values only, please enter a valid date" : "";
+        break;  
+      case "cvc":
+        errors.year = 
+          value.trim().length < 3 || !Number(value) ? "This input accepts numerical values only, please enter a valid date" : "";
+        break;    
+      default: 
         break;
     }
   
     this.setState({errors, [name]: value});
-    this.setState({disable: event.target.value === ''})
+    // this.setState({disable: event.target.value === ''})
   } // handleChange
+
+handleDropdownMonth = (event) => {
+    this.setState({ selectMonth: event.target.value });
+} // handleDropdownMonth
+
+handleDropDownYear = (event) => {
+    this.setState({ selectYear: event.target.value})
+} // handleDropdownYear
 
 handleSubmit = (event) => {
     event.preventDefault();
@@ -83,6 +90,31 @@ render() {
  return (
   <div>
     <form onSubmit={this.handleSubmit} noValidate>
+     <select onChange={this.handleDropdownMonth}>
+        <option value="01">01</option>
+        <option value="02">02</option>
+        <option value="03">03</option>
+        <option value="04">04</option>
+        <option value="05">05</option>
+        <option value="06">06</option>
+        <option value="07">07</option>
+        <option value="08">08</option>
+        <option value="09">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+     </select>
+     <h1>{this.state.selectMonth}</h1>
+     <select onChange={this.handleDropDownYear}>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+     </select>
+     <h1>{this.state.selectYear}</h1>
      <input 
       type = "text"
       placeholder = "Name"
@@ -125,6 +157,8 @@ render() {
       onChange = {this.handleChange}
       noValidate
      />
+     {errors.year.length > 0 && 
+        <span className='error'>{errors.year}</span>}
      <input
       type = "text"
       placeholder = "CVC"
@@ -133,7 +167,9 @@ render() {
       onChange = {this.handleChange}
       noValidate
      />  
-     <button disabled={this.state.disable}>Submit</button>
+     {errors.cvc.length > 0 && 
+        <span className='error'>{errors.cvc}</span>}
+     <button disabled={this.state.title.length < 2 || this.state.number.length < 2 || this.state.month.length < 2 || this.state.year.length < 2 || this.state.cvc.length < 2 ? true : false}>Submit</button>
     </form>
   </div>
   )
